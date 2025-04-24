2f00dd8ef9800e0ae4cd6e0c904e35541f2043be
Santiago Aveiro <118389886+santiaveiro@users.noreply.github.com>
2025-04-24 15:14:37 -0300

    Initial commit

---
+++ src/main/java/com/backendtest/similarproducts/SimilarproductsApplication.java
@@ -0,0 +1,13 @@
+package com.backendtest.similarproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendtest/similarproducts/SimilarproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendtest.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
