f78718a541824c92dd86db4f7e5d71ba9295c9c9
Santiago Aveiro <118389886+santiaveiro@users.noreply.github.com>
2025-04-25 04:20:03 -0300

    feat: implement controller for similar products endpoint

--- src/main/java/com/backendtest/similarproducts/controller/ProductController.java
+++ src/main/java/com/backendtest/similarproducts/controller/ProductController.java
@@ -1,4 +1,23 @@
 package com.backendtest.similarproducts.controller;
 
+import com.backendtest.similarproducts.model.ProductDetail;
+import com.backendtest.similarproducts.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{productId}/similar")
+    public ResponseEntity<List<ProductDetail>> getSimilarProducts(@PathVariable String productId) {
+        List<ProductDetail> similarProducts = productService.getSimilarProducts(productId);
+        return ResponseEntity.ok(similarProducts);
+    }
 }
